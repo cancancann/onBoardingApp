0757184a00478fb9db761bd4e8a27c0440d2568f
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2023-04-13 00:17:03 +0300

    fix: updated app

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import BoardScreen from "./screens/BoardScreen";
+import HomeScreen from "./screens/HomeScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = useState(null);
+
+  const checkIfAppFirstLaunched = async () => {
+    const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+    if (appData == null) {
+      setIsAppFirstLaunched(true);
+      await AsyncStorage.setItem("isAppFirstLaunched", "false");
+    } else {
+      setIsAppFirstLaunched(false);
+    }
+  };
+
+  useEffect(() => {
+    checkIfAppFirstLaunched();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>OnBoardingDemo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    isAppFirstLaunched != null && (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {/* expo cli kullanıyorsanız ve tekrardan slider görüntülemek istiyorsanız
+           !isAppFirstLaunched bu formata getiriniz  */}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          {/* Eğer ReactNative CLi kullanıyorsanız olduğu gibi bırakıp
+            emülatördeki storage kısmını temizlemeniz gerekiyor */}
+          {isAppFirstLaunched && (
+            <Stack.Screen name="OnboardingScreen" component={BoardScreen} />
+          )}
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  );
+};
+
+export default App;
